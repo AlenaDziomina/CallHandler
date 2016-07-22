86225c7cdfa07befc7240153e6dd329f3eadab7e
Alena_Grouk <groukhelena@gmail.com>
2016-07-22 11:41:52 +0000

    PhoneCall dao

---
+++ callhandler/dao/src/main/java/by/grouk/callhandler/dao/impl/PhoneCallDaoImpl.java
@@ -0,0 +1,16 @@
+package by.grouk.callhandler.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import by.grouk.callhandler.dao.PhoneCallDao;
+import by.grouk.callhandler.model.PhoneCall;
+
+/**
+ * Created by Alena_Grouk on 7/22/2016.
+ */
+@Repository
+public class PhoneCallDaoImpl implements PhoneCallDao {
+    public void addCall(PhoneCall call) {
+        //todo
+    }
+}
