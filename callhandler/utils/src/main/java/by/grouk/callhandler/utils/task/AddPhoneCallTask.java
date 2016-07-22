e0cc2ab85fc9a2b44142332dda4ae15673c07db2
Alena_Grouk <groukhelena@gmail.com>
2016-07-22 14:50:47 +0000

    TemplateCode

--- callhandler/utils/src/main/java/by/grouk/callhandler/utils/task/AddPhoneCallTask.java
+++ callhandler/utils/src/main/java/by/grouk/callhandler/utils/task/AddPhoneCallTask.java
@@ -2,7 +2,7 @@ package by.grouk.callhandler.utils.task;
 
 import java.util.concurrent.RecursiveAction;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.Resource;
 
 import by.grouk.callhandler.model.PhoneCall;
 import by.grouk.callhandler.utils.template.PhoneCallTemplateUtil;
@@ -13,11 +13,12 @@ import by.grouk.callhandler.utils.template.generator.TemplateGenerator;
  * Created by Alena_Grouk on 7/22/2016.
  */
 public class AddPhoneCallTask extends RecursiveAction {
+
     private PhoneCall phoneCall;
 
-    @Autowired
+    @Resource
     @TemplateCode(1)
-    TemplateGenerator<PhoneCall> templateGenerator;
+    TemplateGenerator templateGenerator;
 
     public AddPhoneCallTask(PhoneCall phoneCall) {
         this.phoneCall = phoneCall;
