590a0188c74dc9982b09f0aa36d4047c38b72df5
Alena_Grouk <groukhelena@gmail.com>
2016-07-25 16:26:30 +0000

    Autowired constructor

--- callhandler/utils/src/main/java/by/grouk/callhandler/utils/task/AddPhoneCallTask.java
+++ callhandler/utils/src/main/java/by/grouk/callhandler/utils/task/AddPhoneCallTask.java
@@ -2,24 +2,39 @@ package by.grouk.callhandler.utils.task;
 
 import java.util.concurrent.RecursiveAction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
 import by.grouk.callhandler.model.MessageTemplate;
 import by.grouk.callhandler.model.PhoneCall;
+import by.grouk.callhandler.utils.template.PhoneCallTemplateUtil;
+import by.grouk.callhandler.utils.template.TemplateGeneratorFactory;
 import by.grouk.callhandler.utils.template.generator.TemplateGenerator;
 
 /**
  * Created by Alena_Grouk on 7/22/2016.
  */
+@Component
+@Scope("prototype")
 public class AddPhoneCallTask extends RecursiveAction {
 
-    private PhoneCall phoneCall;
-    private TemplateGenerator templateGenerator;
+    @Autowired
+    private TemplateGeneratorFactory generatorFactory;
 
-    public AddPhoneCallTask(PhoneCall phoneCall, TemplateGenerator templateGenerator) {
+    @Autowired
+    private PhoneCallTemplateUtil templateUtil;
+
+    private PhoneCall phoneCall;
+
+
+    public AddPhoneCallTask(PhoneCall phoneCall) {
         this.phoneCall = phoneCall;
-        this.templateGenerator = templateGenerator;
     }
 
     @Override protected void compute() {
+        String templateCode = templateUtil.determineTemplateCode(phoneCall);
+        TemplateGenerator templateGenerator = generatorFactory.getTemplateGenerator(templateCode);
         MessageTemplate template = templateGenerator.generateTemplate(phoneCall);
         //todo
     }
--- callhandler/utils/src/main/java/by/grouk/callhandler/utils/task/manager/AddCallTaskManager.java
+++ callhandler/utils/src/main/java/by/grouk/callhandler/utils/task/manager/AddCallTaskManager.java
@@ -7,9 +7,6 @@ import org.springframework.stereotype.Component;
 
 import by.grouk.callhandler.model.PhoneCall;
 import by.grouk.callhandler.utils.task.AddPhoneCallTask;
-import by.grouk.callhandler.utils.template.PhoneCallTemplateUtil;
-import by.grouk.callhandler.utils.template.TemplateGeneratorFactory;
-import by.grouk.callhandler.utils.template.generator.TemplateGenerator;
 
 /**
  * Created by Alena_Grouk on 7/21/2016.
@@ -21,15 +18,10 @@ public class AddCallTaskManager {
     private ForkJoinPool pool;
 
     @Autowired
-    private TemplateGeneratorFactory generatorFactory;
-
-    @Autowired
-    private PhoneCallTemplateUtil templateUtil;
+    private AddPhoneCallTask callTask;
 
     public void runTask(PhoneCall call) {
-        String templateCode = templateUtil.determineTemplateCode(call);
-        TemplateGenerator templateGenerator = generatorFactory.getTemplateGenerator(templateCode);
-        AddPhoneCallTask addCallTask = new AddPhoneCallTask(call, templateGenerator);
+        AddPhoneCallTask addCallTask = new AddPhoneCallTask(call);
         pool.execute(addCallTask);
     }
 }
