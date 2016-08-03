a935c785e5cc6f256f38b43e986c885011fab015
Alena Grouk <groukhelena@gmail.com>
2016-08-03 09:03:50 +0000

    Test

--- callhandler/utils/src/test/java/by/grouk/callhandler/utils/writer/FileWriterTest.java
+++ callhandler/utils/src/test/java/by/grouk/callhandler/utils/writer/FileWriterTest.java
@@ -1,30 +1,35 @@
 package by.grouk.callhandler.utils.writer;
 
-import by.grouk.callhandler.model.MessageTemplate;
-import by.grouk.callhandler.utils.exception.processing.factory.ExceptionFactory;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.api.support.membermodification.MemberModifier;
-import org.powermock.modules.junit4.PowerMockRunner;
+import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 
-import javax.annotation.Resource;
-
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.concurrent.locks.Lock;
 
-import static org.junit.Assert.*;
-import static org.powermock.api.mockito.PowerMockito.mock;
-import static org.powermock.api.mockito.PowerMockito.when;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.support.membermodification.MemberModifier;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import by.grouk.callhandler.model.Content;
+import by.grouk.callhandler.model.Destination;
+import by.grouk.callhandler.model.MessageTemplate;
+import by.grouk.callhandler.utils.exception.processing.factory.ExceptionFactory;
 
 /**
  * Created by Alena on 03.08.2016.
  */
 @RunWith(PowerMockRunner.class)
+@PrepareForTest(FileWriter.class)
 public class FileWriterTest {
 
     private FileWriter fileWriter;
@@ -39,46 +44,83 @@ public class FileWriterTest {
         exceptionFactory = mock(ExceptionFactory.class);
         MemberModifier.field(FileWriter.class, "fileLockPool").set(fileWriter , fileLockPool);
         MemberModifier.field(FileWriter.class, "exceptionFactory").set(fileWriter , exceptionFactory);
+
+        mockStatic(Paths.class);
+        mockStatic(Files.class);
     }
 
     @Test
-    public void write() throws Exception {
+    public void writeAppend() throws Exception {
         MessageTemplate template = mock(MessageTemplate.class);
+        Destination dest = mock(Destination.class);
+        Content content = mock(Content.class);
         String name = "name";
-        String charsetName = "charset";
+        String charsetName = "utf-8";
         String filePath = "filePath";
         LockedObject lockedPath = mock(LockedObject.class);
         Lock lock = mock(Lock.class);
+        Path path = mock(Path.class);
+        String msg = mock(String.class);
+        byte[] bytes = new byte[]{};
 
-        when(template.getDestination().getFileName()).thenReturn(name);
-        when(template.getDestination().getCharset()).thenReturn(charsetName);
-        when(template.getDestination().getFilePath()).thenReturn(filePath);
+        when(template.getDestination()).thenReturn(dest);
+        when(dest.getFileName()).thenReturn(name);
+        when(dest.getCharset()).thenReturn(charsetName);
+        when(dest.getFilePath()).thenReturn(filePath);
         when(fileLockPool.getLockedObject(name, filePath)).thenReturn(lockedPath);
         when(lockedPath.getLock()).thenReturn(lock);
         when(lockedPath.getObject()).thenReturn(filePath);
+        when(Paths.get(filePath)).thenReturn(path);
+        when(Files.exists(path)).thenReturn(true);
+        when(template.getContent()).thenReturn(content);
+        when(content.getMessage()).thenReturn(msg);
+        when(msg.getBytes(charsetName)).thenReturn(bytes);
 
+        fileWriter.write(template);
 
-        String name = template.getDestination().getFileName();
-        String charsetName = template.getDestination().getCharset();
-        LockedObject<String> lockedPath = fileLockPool.getLockedObject(name, template.getDestination().getFilePath());
-        Lock lock = lockedPath.getLock();
-        String filePath = lockedPath.getObject();
-        try {
-            lock.lock();
-            Path path = Paths.get(filePath);
-            if (!Files.exists(path)) {
-                String msg = template.getContent().getHeader() + template.getContent().getMessage();
-                Files.write(path, msg.getBytes(charsetName), StandardOpenOption.CREATE);
-            } else {
-                String msg = template.getContent().getMessage();
-                Files.write(path, msg.getBytes(charsetName), StandardOpenOption.APPEND);
-            }
-        } catch (IOException e) {
-            throw exceptionFactory.createInternalException(e);
-        } finally {
-            lock.unlock();
-        }
+        verify(lock).lock();
+        verifyStatic(times(1));
+        Files.write(path, bytes, StandardOpenOption.APPEND);
+        verify(lock).unlock();
+    }
 
+    @Test
+    public void writeCreate() throws Exception {
+        MessageTemplate template = mock(MessageTemplate.class);
+        Destination dest = mock(Destination.class);
+        Content content = mock(Content.class);
+        String name = "name";
+        String charsetName = "utf-8";
+        String filePath = "filePath";
+        LockedObject lockedPath = mock(LockedObject.class);
+        Lock lock = mock(Lock.class);
+        Path path = mock(Path.class);
+        String msg = mock(String.class);
+        String header = mock(String.class);
+        String msg2 = mock(String.class);
+        byte[] bytes = new byte[]{};
+
+        when(template.getDestination()).thenReturn(dest);
+        when(dest.getFileName()).thenReturn(name);
+        when(dest.getCharset()).thenReturn(charsetName);
+        when(dest.getFilePath()).thenReturn(filePath);
+        when(fileLockPool.getLockedObject(name, filePath)).thenReturn(lockedPath);
+        when(lockedPath.getLock()).thenReturn(lock);
+        when(lockedPath.getObject()).thenReturn(filePath);
+        when(Paths.get(filePath)).thenReturn(path);
+        when(Files.exists(path)).thenReturn(false);
+        when(template.getContent()).thenReturn(content);
+        when(content.getMessage()).thenReturn(msg);
+        when(content.getHeader()).thenReturn(header);
+        when(msg.concat(header)).thenReturn(msg2);
+        when(msg2.getBytes(charsetName)).thenReturn(bytes);
+
+        fileWriter.write(template);
+
+        verify(lock).lock();
+        verifyStatic(times(1));
+        Files.write(path, bytes, StandardOpenOption.CREATE);
+        verify(lock).unlock();
     }
 
 }
